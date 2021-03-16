00adac083edb55f065c60db7c5d611d3291461a2
Beef <ethandavidbarrow@outlook.com>
2021-03-16 22:36:38 +0000

    fix gitignore and also plugin soontm

---
+++ core/addons/plugins/zpma/index.cmd
@@ -0,0 +1,15 @@
+:: Name: zpma
+:: Description: An addon manager for ZMD.
+:: Usage: zpma
+:: Author: rem
+
+:: ZPMA is a proof of concept for what ZMD plugins can do.
+:: It includes:
+:: i18n
+:: API functions
+:: External requests
+
+:: TODO: Add all of this
+
+:input
+
