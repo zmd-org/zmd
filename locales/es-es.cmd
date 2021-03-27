448e74fda82e736c1484356fb421e36637ca9abf
Angel Hiraldo <ahiraldo41@gmail.com>
2021-03-26 21:00:07 -0400

    Spanish translation

---
+++ locales/es-es.cmd
@@ -0,0 +1,27 @@
+:: ZMD, a Windows command line.
+:: British English translations.
+
+:: Main Program Translations
+set i18nWelcome=Bienvenidos a ZMD!
+set i18nInvalidCommand=Este comando no esta valido! Para la lista de Comandos validos, escribe "help".
+
+:: Built-in Command Translations
+:: "help"
+set i18nAllCommands=Comandos
+set i18nBuiltins=Built-ins
+set i18nPlugins=Plugins
+set i18nName=Name
+set i18nDescription=Descripcion
+set i18nUsage=Uso
+set i18nAuthor=Autor
+
+:: "settings"
+set i18nSettingsBanner=Que configuracion quieres cambiar?
+set i18nValidSettings=Configuraciones validas son
+set i18nTypeSetting=Escribe una configuracion
+set i18nChosenSetting=Escojiste al cambiar esta configuracion
+set i18nWhatValue=Que valor quieres cambiar en esta configuracion?
+set i18nCurrentValue=El valor actual es
+set i18nValidValues=Valores validos son
+set i18nTypeValue=Escribe un valor aqui
+set i18nInvalidValue=Eso no es un valor valido!
